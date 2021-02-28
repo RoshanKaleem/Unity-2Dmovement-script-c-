df7f669dcfba18a09287aedb938964ca5f64e24c
RoshanKaleem <65737218+RoshanKaleem@users.noreply.github.com>
2021-02-28 09:15:26 -0800

    Simple 2d movement
    Simple 2d movement script for movement on the x-axis.
    Horizontal movement.

---
+++ movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float speed = 3f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          Move();
+    }
+    void Move()
+    {
+
+        float h = Input.GetAxisRaw("Horizontal");
+
+        Vector3 temp = transform.position;
+
+        if (h > 0)
+        {
+            // going to the right side
+
+            temp.x += speed * Time.deltaTime;
+
+
+
+        }
+        else if (h < 0)
+        {
+
+            temp.x -= speed * Time.deltaTime;
+
+
+        }
+
+
+        transform.position = temp;
+
+    }
+
+}
