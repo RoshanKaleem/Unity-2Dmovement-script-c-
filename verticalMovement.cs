99d36a40332dd6ef142b71fad9feae66d1d3a636
RoshanKaleem <65737218+RoshanKaleem@users.noreply.github.com>
2021-02-28 10:11:34 -0800

    Vertical movement Y axis

---
+++ verticalMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class verticalMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float speed = 3f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          Move();
+    }
+    void Move()
+    {
+
+        float h = Input.GetAxisRaw("Vertical");
+
+        Vector3 temp = transform.position;
+
+        if (h > 0)
+        {
+            // going up
+
+            temp.y += speed * Time.deltaTime;
+
+
+
+        }
+        else if (h < 0)
+        {
+       //going down
+            temp.y -= speed * Time.deltaTime;
+
+
+        }
+
+
+        transform.position = temp;
+
+    }
+
+}
